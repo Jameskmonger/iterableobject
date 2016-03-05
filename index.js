76d3e0d0afad4c535d5698ab951af44b7f63b39a
James Monger <jameskmonger@hotmail.co.uk>
2016-03-05 14:04:18 +0000

    Throw type error

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 (function(mod) {
     mod.exports = function (obj) {
+        if (typeof obj !== 'object') {
+            throw new Error('Cannot create an iterable from that type.');
+        }
+
         throw new Error("not implemented");
     };
 })(module);
