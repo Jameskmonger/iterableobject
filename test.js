73cb57c8ea8935c6e9dcc2128c32b111244871ad
James Monger <jameskmonger@hotmail.co.uk>
2016-03-05 14:06:01 +0000

    Test error for null

--- test.js
+++ test.js
@@ -73,3 +73,20 @@ const iterableobject = require('./index.js');
 
     console.log("PASS: " + testName);
 })();
+
+(() => {
+    let testName = 'should throw error trying to create iterable from null';
+
+    let errorThrown = false;
+    try {
+        iterableobject(null);
+    } catch (e) {
+        errorThrown = true;
+    }
+
+    if (!errorThrown) {
+        throw new Error("FAIL: " + testName);
+    }
+
+    console.log("PASS: " + testName);
+})();
