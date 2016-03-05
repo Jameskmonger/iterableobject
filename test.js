c1b15dcdd1a1308d880fb405397285862360eea1
James Monger <jameskmonger@hotmail.co.uk>
2016-03-05 13:56:26 +0000

    test throwing errror for number

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
 var iterableobject = require('./index.js');
+
+(function() {
+    var testName = 'should throw error trying to create iterable from number';
+
+    var errorThrown = false;
+    try {
+        iterableobject(0);
+    } catch (e) {
+        errorThrown = true;
+    }
+
+    if (!errorThrown) {
+        throw new Error("FAIL: " + testName);
+    }
+
+    console.log("PASS: " + testName);
+})();
